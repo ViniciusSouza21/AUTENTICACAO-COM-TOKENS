54c1fba7f805d1ed0a84a906d828cd111fcdec95
ViniciusSouza21 <79486692+ViniciusSouza21@users.noreply.github.com>
2022-06-29 14:33:06 -0300

    Senha Criptografada

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -1,10 +1,13 @@
 import usuarios from "../models/Usuario.js";
+import bcrypt from "bcrypt";
 
 class UsuariosController {
 
     static cadastrarUsuario = (req, res) => {
         let usuario = new usuarios (req.body);
+        let senha = req.body.senha;
 
+        usuario.senha = bcrypt.hashSync(senha, 8);
         usuario.save((err) => {
             if (err) {
                 res.status(500).send({message: `${err.message} - Falha ao cadastrar o usuário.`});
